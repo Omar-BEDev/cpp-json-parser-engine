31a9c036da89cb47859cde3c0114d7ce872826d7
Omar-BEDev <kaliochibrahim@gmail.com>
2026-08-31 01:22:23 +0000

    fix(error-handling): didn't add cstdio lib mistake

--- src/error-handling/error-handling.cpp
+++ src/error-handling/error-handling.cpp
@@ -1,17 +1,17 @@
 #include "error-handling.h"
+#include <cstdio>
 #include <string_view>
 #include <unordered_map>
-
-std::unordered_map<ErrorType, std::string_view> errorMessage {
+std::unordered_map<ErrorType, std::string_view> errorMessage{
     {INVALID_ESCAPE_SEQUENCE, "invalid escape sequence"},
     {INVALID_HEX_VALUE, "invalid hex value"},
 };
 
 void print_error_message(ErrorType errorType, int line, int column) {
-    auto it = errorMessage.find(errorType);
-    if (it == errorMessage.end()) {
-        printf("undefined error");
-        return;
-    }
-    printf("%s [Ln %d ,Col  %d]", it->second.data(), line, column);
+  auto it = errorMessage.find(errorType);
+  if (it == errorMessage.end()) {
+    printf("undefined error");
+    return;
+  }
+  printf("%s [Ln %d ,Col  %d]", it->second.data(), line, column);
 }
